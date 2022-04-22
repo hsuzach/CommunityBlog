f2962ef6798ebc6cc87f11f9e7e29a1a78a7b0f2
hsuzach <hsuzach@gmail.com>
2022-04-21 23:58:01 -0700

    test for deploy

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const User = require('../../models/user.js');
+const { User } = require('../models/');
 
 //create a new user with credentials
 //logs in user after signing up
